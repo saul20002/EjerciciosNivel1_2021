35ae385e5489937796b16156835e5d59afa16867
Gaming JS <Gaming JS@DESKTOP-4JD9KQQ>
2021-01-21 13:20:48 +0100

    Ejercicio a medio hacer

--- src/ejerciciosnivel1_2021/EjerciciosNivel1_2021.java
+++ src/ejerciciosnivel1_2021/EjerciciosNivel1_2021.java
@@ -11,11 +11,27 @@ package ejerciciosnivel1_2021;
  */
 public class EjerciciosNivel1_2021 {
 
+    //declaro un array de integers para hacer pruebas   
+    int[] listaNumeros = {3, 31, 200, 20, 5, 99};
+    
+    public int maximo(int[] lista){
+        int auxiliar = lista[0]; //el máximo va a ser el primer elemento del array
+        for (int i=0; i< lista.length; i++) {
+            if (auxiliar < lista[1]){
+                auxiliar = lista[1];
+            }
+        }
+        return auxiliar;
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        EjerciciosNivel1_2021 ejercicio = new EjerciciosNivel1_2021();
+        System.out.println (ejercicio.listaNumeros[0]);
+        System.out.println(ejercicio.maximo(ejercicio.listaNumeros));
+        
     }
     
 }
